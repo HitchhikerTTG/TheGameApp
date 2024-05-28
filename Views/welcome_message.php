c4fde34061b1ae135b705623843063a850e940a4
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-05-28 16:37:48 +0200

    Zmuszanie do wspolpracy
    Kolejny etap, praca na niewidocznym to webhook

--- Views/welcome_message.php
+++ Views/welcome_message.php
@@ -225,7 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to WitIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Welcome to Wit-o-Igniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
