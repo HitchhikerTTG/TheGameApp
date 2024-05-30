2b013a6f76d47e92da5af38984a5038339883a12
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-05-30 12:13:52 +0200

    Poprawka technicna
    Przekllejka

--- Models/TurniejeModel.php
+++ Models/TurniejeModel.php
@@ -19,14 +19,14 @@ class TurniejeModel extends Model{
     }
     
     public function zmienAktywnyTurniej($aktywnyTurniejId) {
-23        // Ustawienie wszystkich turniejów na nieaktywne
-24        $this->where('Active', 1)->set('Active', 0)->update();
-25
-26        // Ustawienie wybranego turnieju na aktywny
-27        $this->update($aktywnyTurniejId, ['Active' => 1]);
-28
-29        // Pobranie danych aktywnego turnieju
-30        return $this->find($aktywnyTurniejId);
-31    }
+        // Ustawienie wszystkich turniejów na nieaktywne
+        $this->where('Active', 1)->set('Active', 0)->update();
+
+        // Ustawienie wybranego turnieju na aktywny
+        $this->update($aktywnyTurniejId, ['Active' => 1]);
+
+        // Pobranie danych aktywnego turnieju
+        return $this->find($aktywnyTurniejId);
+    }
 
 }
