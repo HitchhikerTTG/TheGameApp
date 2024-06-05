11a74892d1e1c3243722d49d7e9506a4f2c325f8
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-06-05 15:00:54 +0200

    inne podejście

--- Views/ukladanka/sg/SkryptTypowania.php
+++ Views/ukladanka/sg/SkryptTypowania.php
@@ -1,9 +1,8 @@
+<div id="fileContent"></div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <div id="fileContent"></div>
-
     <script>
         $(document).ready(function() {
-            const fileUrl = 'https://nirski.com/ksiazki.html'; // Zmień ścieżkę na odpowiednią
+            const fileUrl = '/data.txt'; // Zmień ścieżkę na odpowiednią
 
             $.get(fileUrl, function(data) {
                 console.log('Data fetched:', data);
