e910e43de7b1eaa2473e45a3832aabbd51609bdb
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-06-05 15:06:44 +0200

    Spróbujmy tak pobrać ten plik

--- Views/ukladanka/sg/SkryptTypowania.php
+++ Views/ukladanka/sg/SkryptTypowania.php
@@ -2,7 +2,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
-            const fileUrl = 'data.txt'; // Zmień ścieżkę na odpowiednią
+            const fileUrl = '/data.txt'; // Zmień ścieżkę na odpowiednią
 
             $.get(fileUrl, function(data) {
                 console.log('Data fetched:', data);
