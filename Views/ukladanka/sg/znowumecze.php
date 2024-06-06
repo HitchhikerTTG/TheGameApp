87f97d31fa7fac7ca4182223ae41a83e27538b8e
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-06-06 12:42:08 +0200

    Odwzorowanie danych ze skryptu do php

--- Views/ukladanka/sg/znowumecze.php
+++ Views/ukladanka/sg/znowumecze.php
@@ -1,20 +1,112 @@
 <div class="container mt-3 px-0 mx-0">
-        <div id="matchesAccordion">
+<div id="matchesAccordion">
     <?php foreach ($mecze as $match): ?>
         <div class="accordion-item">
             <h2 class="accordion-header" id="heading<?= $match['ApiID']; ?>">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $match['ApiID']; ?>" aria-expanded="false" aria-controls="collapse<?= $match['ApiID']; ?>">
-                    <?= $match['details']['home_team']['name'] ?? 'Unknown'; ?> vs <?= $match['details']['away_team']['name'] ?? 'Unknown'; ?>
+                    <?= $match['details']['home_team']['name'] ?? 'Unknown'; ?> vs <?= $match['details']['away_team']['name'] ?? 'Unknown'; ?> | Wytypuj
                 </button>
             </h2>
             <div id="collapse<?= $match['ApiID']; ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $match['ApiID']; ?>">
                 <div class="accordion-body">
-                    <p>Date: <?= $match['details']['date'] ?? 'N/A'; ?></p>
-                    <p>Time: <?= $match['details']['time'] ?? 'N/A'; ?></p>
-                    <!-- Add more details as needed -->
+                    <div class="row match form-row text-center">
+                        <div class="col">
+                            <form action="https://jakiwynik.com/theGame/nowyZapisTypu" method="post">
+                                <input type="hidden" name="userUID" value="<?= $userID; ?>">
+                                <input type="hidden" name="gameID" value="<?= $match['Id']; ?>">
+                                <input type="hidden" name="turniejID" value="<?= $turniejID; ?>">
+                                <div class="row">
+                                    <div class="col team h_<?= $match['details']['home_team']['id']; ?>">
+                                        <div class="row">
+                                            <div class="col team-name">
+                                                <?= $match['details']['home_team']['name']; ?>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col text-center">
+                                                <div class="score-display"><?= $match['typy']['HomeTyp'] ?? '-'; ?></div>
+                                                <input type="hidden" name="H" class="score-value" value="<?= $match['typy']['HomeTyp'] ?? 0; ?>">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col zminusem"><button type="button" class="minus">-</button></div>
+                                            <div class="col zplusem"><button type="button" class="plus">+</button></div>
+                                        </div>
+                                    </div>
+                                    <div class="col team a_<?= $match['details']['away_team']['id']; ?>">
+                                        <div class="row">
+                                            <div class="col team-name">
+                                                <?= $match['details']['away_team']['name']; ?>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="score-display"><?= $match['typy']['AwayTyp'] ?? '-'; ?></div>
+                                                <input type="hidden" name="A" class="score-value" value="<?= $match['typy']['AwayTyp'] ?? 0; ?>">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col zminusem text-center"><button type="button" class="minus">-</button></div>
+                                            <div class="col zplusem text-center"><button type="button" class="plus">+</button></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <input type="checkbox" id="goldenGame<?= $match['Id']; ?>" name="goldenGame" value="1" <?= $match['isGoldenGame'] ? 'checked' : ''; ?> <?= $usedGoldenBall !== 0 && $usedGoldenBall !== $match['Id'] ? 'disabled' : ''; ?>>
+                                        <label for="goldenGame<?= $match['Id']; ?>">Golden Game</label>
+                                    </div>
+                                </div>
+                                <div class="row text-center">
+                                    <div class="col">
+                                        <button type="submit" class="btn btn-primary">Typuję!</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="row">
+                                <div class="col">
+                                    Liczba typów dla tego meczu: <?= $match['liczbaTypow']; ?>
+                                    <!-- Scrollable modal -->
+                                    <!-- Button trigger modal -->
+                                    <?php if (strtotime($match['details']['date'] . ' ' . $match['details']['time']) < time()): ?>
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#typy<?= $match['Id']; ?>">
+                                            Jak typowali?
+                                        </button>
+                                        <div class="modal fade" id="typy<?= $match['Id']; ?>" tabindex="-1" aria-labelledby="typy<?= $match['Id']; ?>Label" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="typy<?= $match['Id']; ?>Label">Typy innych</h1>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Tu będą widoczne typy wszystkich, którzy wytypowali ten mecz
+                                                    </div>
+                                                    <div class="modal-footer"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col betting-hints">
+                                    <div class="col-12">
+                                        <div class="hints-title">Podpowiedź bookmacherów</div>
+                                        <div class="odds-container">
+                                            <div class="odds">1: <?= $match['details']['odds']['1'] ?? 'N/A'; ?></div>
+                                            <div class="odds">X: <?= $match['details']['odds']['X'] ?? 'N/A'; ?></div>
+                                            <div class="odds">2: <?= $match['details']['odds']['2'] ?? 'N/A'; ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     <?php endforeach; ?>
 </div>
-    </div>
+
+
+
