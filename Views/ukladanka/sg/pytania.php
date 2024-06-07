5aea1535d8a50d83aaa51b7beb69c3f0037a87b3
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-06-07 22:20:49 +0200

    Małe sprawdzające przywrócenie

--- Views/ukladanka/sg/pytania.php
+++ Views/ukladanka/sg/pytania.php
@@ -1,13 +1,13 @@
 <?php foreach ($pytania as $pytanie): ?>
     <div class="pytanie">
         <p><?= esc($pytanie['tresc']) ?></p>
-        <p>Punkty: <?= esc($pytanie['pkt']) ?></p>
+        <p>Punkty za pytanie: <?= esc($pytanie['pkt']) ?></p>
         <p>Ważne do: <?= esc($pytanie['wazneDo']) ?></p>
         <form method="post" action="<?= site_url('TheGame/zapiszOdpowiedzNaPytanie') ?>">
             <input type="hidden" name="pytanieID" value="<?= $pytanie['id'] ?>">
             <input type="hidden" name="uniid" value="<?= session()->get('loggedInUser') ?>">
-            <input type="text" name="odpowiedz" value="<?= isset($pytanie['user_answer']) ? esc($pytanie['user_answer']) : '' ?>" required>
-            <button type="submit"><?= isset($pytanie['user_answer']) ? 'Zmień' : 'Zapisz' ?></button>
+            <input type="text" name="odpowiedz" value="<?= isset($pytanie['dotychczasowa_odpowiedz']) ? esc($pytanie['dotychczasowa_odpowiedz']) : '' ?>" required>
+            <button type="submit">Zapisz</button>
         </form>
     </div>
 <?php endforeach; ?>
