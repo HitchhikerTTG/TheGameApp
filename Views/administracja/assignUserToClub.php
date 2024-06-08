8cc02fd9734bba7458b440a668f09fb3b7911784
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-06-08 13:51:48 +0200

    Sprawdzamy

--- Views/administracja/assignUserToClub.php
+++ Views/administracja/assignUserToClub.php
@@ -18,33 +18,6 @@ if ($sukces) {
 }
 ?>
 
-<h2>Przypisz użytkownika do klubu</h2>
-
-<?= session()->getFlashdata('error') ?>
-<?= service('validation')->listErrors() ?>
-
-<form action="/AdminDash/assignUserToClub" method="post">
-    <?= csrf_field() ?>
-
-    <label for="userID">Użytkownik:</label>
-    <select name="userID">
-        <?php foreach ($users as $user): ?>
-            <option value="<?= $user['uniID'] ?>"><?= $user['nick'] ?></option>
-        <?php endforeach; ?>
-    </select>
-    <br /><br />
-
-    <label for="clubID">Klub:</label>
-    <select name="clubID">
-        <?php foreach ($clubs as $club): ?>
-            <option value="<?= $club['id'] ?>"><?= $club['Nazwa'] ?></option>
-        <?php endforeach; ?>
-    </select>
-    <br /><br />
-
-    <input type="submit" name="submit" value="Przypisz użytkownika do klubu &raquo;" />
-</form>
-
 <h2>Użytkownicy w klubach</h2>
 <table>
     <thead>
