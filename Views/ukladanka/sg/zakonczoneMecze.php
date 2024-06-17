2ddc8e935527c3e0e62a22cd0aa13e35fc0f9a2d
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-06-17 18:52:03 +0200

    Poprawka

--- Views/ukladanka/sg/zakonczoneMecze.php
+++ Views/ukladanka/sg/zakonczoneMecze.php
@@ -49,8 +49,8 @@
                                     <p>Zwycięstwo $homeTeamName wytypowało: <?= $match['podsumowanieTypow']['countWin1']; ?> osób </p>
                                     <p>Zwycięstwo $awayTeamName: <?= $match['podsumowanieTypow']['countWin2']; ?> osób </p>
                                     <p>Remis obstawiło: <?= $match['podsumowanieTypow']['countDraw']; ?> osób </p>
-                                    <p>Najpopularniejszy typ: <?= $match['podsumowanieTypow']['MostPopularType']; ?> osób </p>
-                                    <p>Złota piłka użyta: <?= $match['podsumowanieTypow']['GoldenBallCount']; ?> osób </p>
+                                    <p>Najpopularniejszy typ: <?= $match['podsumowanieTypow']['mostPopularType']; ?> osób </p>
+                                    <p>Złota piłka użyta: <?= $match['podsumowanieTypow']['goldenBallCount']; ?> osób </p>
                                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#typy<?= $match['Id']; ?>">
                                         Jak typowali?
                                     </button>
