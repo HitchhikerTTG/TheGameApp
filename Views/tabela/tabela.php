32430f7d828687b7f5249b2927c200d108a2cbc8
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-06-23 11:16:13 +0200

    Sprawdzanie tabeli

--- Views/tabela/tabela.php
+++ Views/tabela/tabela.php
@@ -4,152 +4,78 @@ document.addEventListener('DOMContentLoaded', function() {
     var userID = <?php echo json_encode($userID); ?>;
     var aktualnyFiltr = 'pelny';
     var widokSkrócony = true; // Domyślnie skrócony widok
-    
-    console.log('Uzytkownik', userID);
 
-    function ustalPozycje(dane, filtr) {
+    function ustalPozycje(dane) {
+        dane.sort((a, b) => b.punkty - a.punkty);
+
+        let pozycje = [];
         let aktualnaPozycja = 1;
-        dane[0].wyswietlanaPozycja = aktualnaPozycja;
-        let graczeNaPozycji = 1;
 
-        for (let i = 1; i < dane.length; i++) {
-            if (dane[i][filtr] === dane[i - 1][filtr]) {
-                dane[i].wyswietlanaPozycja = '-';
-                graczeNaPozycji++;
-            } else {
-                aktualnaPozycja += graczeNaPozycji;
-                dane[i].wyswietlanaPozycja = aktualnaPozycja;
-                graczeNaPozycji = 1;
+        for (let i = 0; i < dane.length; i++) {
+            if (i === 0 || dane[i].punkty !== dane[i - 1].punkty) {
+                aktualnaPozycja = i + 1;
             }
-        }
-    }
-
-    function sortujIDodajPozycje(filtr) {
-        if (filtr === 'pelny') {
-            tabelaDanych.sort((a, b) => b.punkty - a.punkty);
-            ustalPozycje(tabelaDanych, 'punkty');
-        } else {
-            tabelaDanych.sort((a, b) => b[filtr] - a[filtr]);
-            ustalPozycje(tabelaDanych, filtr);
-        }
-    }
-
-function generujTabele(filtr) {
-    sortujIDodajPozycje(filtr);
-    var html = '<table class="table">';
-    html += '<tr><th>#</th><th>Nick</th>';
-
-    switch (filtr) {
-        case 'pelny':
-            html += '<th class="text-center">Punkty</th>';
-            break;
-        case 'punktyZaMecze':
-            html += '<th class="text-center">Punkty</th><th class="text-end">Dokładny wynik</th>';
-            break;
-        case 'dokladneTrafienia':
-            html += '<th>Dokładne</th>';
-            break;
-        case 'punktyZaPytania':
-            html += '<th>Punkty</th>';
-            break;
-    }
-
-    html += '</tr>';
-
-    // Określenie pozycji użytkownika zgodnie z pełnym widokiem
-    let pozycjaUzytkownikaWPelnejTabeli = tabelaDanych.findIndex(gracz => gracz.uid == userID) + 1;
-    let pozycjaUzytkownika = pozycjaUzytkownikaWPelnejTabeli; // Użyjemy tej zmiennej do wyświetlenia pozycji użytkownika
-
-    let limit = widokSkrócony ? 10 : tabelaDanych.length;
-    tabelaDanych.slice(0, limit).forEach((gracz, index) => {
-        let klasaStylu = gracz.uid == userID ? 'class="user-row"' : '';
-        let wyswietlanaPozycja = gracz.wyswietlanaPozycja === '-' && gracz.uid == userID ? pozycjaUzytkownikaWPelnejTabeli : gracz.wyswietlanaPozycja;
-
-        html += `<tr ${klasaStylu}><td>${wyswietlanaPozycja}</td><td>${gracz.nick}</td>`;
-
-        // Wypisanie wartości kolumn zgodnie z wybranym filtrem
-        switch (filtr) {
-            case 'pelny':
-                html += `<td class="text-center">${gracz.punkty}</td>`;
-                break;
-            case 'punktyZaMecze':
-                html += `<td class="text-center">${gracz.punktyZaMecze}</td><td class="text-end">${gracz.dokladneTrafienia}</td>`;
-                break;
-            case 'dokladneTrafienia':
-                html += `<td>${gracz.dokladneTrafienia}</td>`;
-                break;
-            case 'punktyZaPytania':
-                html += `<td class="text-end">${gracz.punktyZaPytania}</td>`;
-                break;
+            pozycje.push({
+                uid: dane[i].uid,
+                nick: dane[i].nick,
+                punkty: dane[i].punkty,
+                pozycja: (i === 0 || dane[i].punkty !== dane[i - 1].punkty) ? aktualnaPozycja : '-'
+            });
         }
 
-        html += `</tr>`;
-    });
+        return pozycje;
+    }
 
-// Dodanie dodatkowego wiersza dla użytkownika w skróconej wersji tabeli, jeśli jest poza top 10
-if (widokSkrócony && pozycjaUzytkownikaWPelnejTabeli > 10) {
-    html += '<tr><td colspan="5">&nbsp;</td></tr>'; // Pusty wiersz dla oddzielenia
-    let daneUzytkownika = tabelaDanych.find(gracz => gracz.uid == userID);
-    if (daneUzytkownika) {
-        html += `<tr class="user-row"><td>${pozycjaUzytkownika}</td><td>${daneUzytkownika.nick}</td>`;
+    function generujTabele(pozycje) {
+        var html = '<table class="table">';
+        html += '<tr><th>#</th><th>Nick</th><th class="text-center">Punkty</th></tr>';
 
-        // Wypisanie wartości kolumn zgodnie z wybranym filtrem
-        switch (filtr) {
-            case 'pelny':
-                html += `<td class="text-center">${daneUzytkownika.punkty}</td>`;
-                break;
-            case 'punktyZaMecze':
-                html += `<td class="text-center">${daneUzytkownika.punktyZaMecze}</td><td class="text-end">${daneUzytkownika.dokladneTrafienia}</td>`;
-                break;
-            case 'dokladneTrafienia':
-                html += `<td>${daneUzytkownika.dokladneTrafienia}</td>`;
-                break;
-            case 'punktyZaPytania':
-                html += `<td class="text-end">${daneUzytkownika.punktyZaPytania}</td><td></td>`;
-                break;
+        let pozycjaUzytkownika = pozycje.find(p => p.uid == userID).pozycja;
+        let limit = widokSkrócony ? 10 : pozycje.length;
+
+        pozycje.slice(0, limit).forEach(gracz => {
+            let klasaStylu = gracz.uid == userID ? 'class="user-row"' : '';
+            html += `<tr ${klasaStylu}><td>${gracz.pozycja}</td><td>${gracz.nick}</td><td class="text-center">${gracz.punkty}</td></tr>`;
+        });
+
+        if (widokSkrócony && pozycjaUzytkownika > 10) {
+            html += '<tr><td colspan="3">&nbsp;</td></tr>'; // Pusty wiersz dla oddzielenia
+            let daneUzytkownika = pozycje.find(p => p.uid == userID);
+            html += `<tr class="user-row"><td>${daneUzytkownika.pozycja}</td><td>${daneUzytkownika.nick}</td><td class="text-center">${daneUzytkownika.punkty}</td></tr>`;
         }
 
-        html += `</tr>`;
+        html += '</table>';
+        document.getElementById('tabelaGraczyContainer').innerHTML = html;
     }
-}
-
-    html += '</table>';
-    document.getElementById('tabelaGraczyContainer').innerHTML = html;
-}
 
     document.querySelectorAll('.filtr').forEach(link => {
         link.addEventListener('click', function(event) {
-            event.preventDefault(); // Zapobiegaj przeładowaniu strony
-            document.querySelectorAll('.filtr').forEach(lnk => lnk.classList.remove('active')); // Usuń klasę 'active' z wszystkich zakładek
-            this.classList.add('active'); // Dodaj klasę 'active' do klikniętej zakładki
+            event.preventDefault();
+            document.querySelectorAll('.filtr').forEach(lnk => lnk.classList.remove('active'));
+            this.classList.add('active');
             aktualnyFiltr = this.dataset.filtr;
-            generujTabele(aktualnyFiltr);
+            let pozycje = ustalPozycje(tabelaDanych);
+            generujTabele(pozycje);
         });
     });
-
 
     document.getElementById('przelacznikWidoku').addEventListener('click', function() {
         widokSkrócony = !widokSkrócony;
         this.innerText = widokSkrócony ? "Rozwiń tabelę" : "Zwiń tabelę";
-        generujTabele(aktualnyFiltr);
+        let pozycje = ustalPozycje(tabelaDanych);
+        generujTabele(pozycje);
     });
 
-    generujTabele('pelny'); // Generuj pełną tabelę przy pierwszym ładowaniu
+    let pozycje = ustalPozycje(tabelaDanych);
+    generujTabele(pozycje);
 });
 </script>
-
-<!-- Przyciski filtrów 
-<button class="filtr" data-filtr="pelny">Ogółem</button>
-<button class="filtr" data-filtr="punktyZaMecze">Mecze</button>
-<button class="filtr" data-filtr="dokladneTrafienia">Dokładne</button>
-<button class="filtr" data-filtr="punktyZaPytania">Pytania</button>
--->
 
 <style>
 .miejsce-1 { font-weight: bold; }
 .miejsce-2 { font-style: italic; }
 .miejsce-3 { text-decoration: underline; }
-.user-row { background-color: #ffff99; } /* Wyróżnienie wiersza użytkownika */
+.user-row { background-color: #ffff99; }
 </style>
 
 <div class="row">
@@ -160,7 +86,7 @@ if (widokSkrócony && pozycjaUzytkownikaWPelnejTabeli > 10) {
 </div>
 <ul class="nav nav-tabs">
   <li class="nav-item">
-    <a class="nav-link active filtr" data-filtr="pelny" href="#">Ogółem </a>
+    <a class="nav-link active filtr" data-filtr="pelny" href="#">Ogółem</a>
   </li>
   <li class="nav-item">
     <a class="nav-link filtr" data-filtr="punktyZaMecze" href="#">Mecze</a>
@@ -175,10 +101,4 @@ if (widokSkrócony && pozycjaUzytkownikaWPelnejTabeli > 10) {
 </div>
 </div>
 
-<!-- Przycisk przełączania między pełnym a skróconym widokiem -->
 <button id="przelacznikWidoku">Rozwiń tabelę</button>
-
-
-<pre>
-<?/* print_r($tabelaDanych); */?>
-</pre>
