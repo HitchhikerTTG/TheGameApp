9b7910f5524d18be7f0d15d979bc7ad3be56ef02
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2025-01-26 13:42:48 +0000

    Assistant checkpoint: Restore working naZywo.php with event handling
    Assistant generated file changes:
    - Views/live/naZywo.php: Restore working version with event handling
    
    ---
    
    User prompt:
    
    Just so you know, this version of "naZywo.php" worked:

--- Views/live/naZywo.php
+++ Views/live/naZywo.php
@@ -1,15 +1,94 @@
 
-<div>
-<?php if (isset($live) && is_array($live)) { ?>
 <div id="TrwajaceMecze">
-<?php foreach ($live as $_score) { ?>
-      <button style="width:100%; margin-bottom: 5px;" id="mecz_<?=$_score['id'] ?? ''?>" class="collapsible">      
-        <p class="h"><?=isset($_score['competition']['name']) ? $_score['competition']['name'] : 'Unknown Competition'?></p>
-        <p class="a"><?=$_score['home_name'] ?? ''?> <?=$_score['score'] ?? ''?> <?=$_score['away_name'] ?? ''?></p>
+
+<?php 
+foreach ($live as $_score){
+$tabelaWydarzen=$_score['wydarzenia'];
+   
+if ($tabelaWydarzen['event']){
+
+ ?>
+<button class="collapsible rozgrywki_<?=$_score['competition_id']?>" id="m_<?=$_score['id']?>>"><?=$_score['home_name']?> <?=$_score['score']?> <?=$_score['away_name']?> | <?=$_score['time']?>'
+      </button>
+      <div class="content" id="<?=$_score['id']?>">
+<p align="left" style="font-size:11px; padding-left: 10px; margin:5px">Rozgrywki: <?=$_score['competition_name']?> (<?=$_score['competition_id']?> )</p>
+			<?php 
+			
+      foreach ($tabelaWydarzen['event'] as $wydarzenie){
+        
+        switch($wydarzenie['event']){
+          case "GOAL":
+                  echo "<p class=".$wydarzenie['home_away']."><span alt=\"GOL GOL GOL dla Mlodych Szakali\" title=\"GOL GOL GOL dla Mlodych Szakali\">⚽️ ".$wydarzenie['time']."': <span class=\"gracz goal\">".strtolower($wydarzenie['player'])."</span></p>";          
+                  break;
+          case "YELLOW_CARD":
+                  echo "<p class=".$wydarzenie['home_away']."><span alt=\"Żółta kartka\">🟡 ".$wydarzenie['time']."': <span class=\"gracz\">".strtolower($wydarzenie['player'])."</span></p>";          
+                  break;
+          case "RED_CARD":
+                  echo "<p class=".$wydarzenie['home_away'].">🔴 ".$wydarzenie['time']."': <span class=\"gracz\">".strtolower($wydarzenie['player'])."</span></p>";          
+                  break;
+          case "YELLOW_RED_CARD":
+                  echo "<p class=".$wydarzenie['home_away'].">🟡🔴 ".$wydarzenie['time']."': <span class=\"gracz\">".strtolower($wydarzenie['player'])."</span></p>";          
+                  break;
+                    case "GOAL_PENALTY":
+                  echo "<p class=".$wydarzenie['home_away'].">⚽️<sup>K</sup> ".$wydarzenie['time']."': <span class=\"gracz goal\">".strtolower($wydarzenie['player'])."</span></p>";          
+                  break;
+          case "OWN_GOAL":
+                  echo "<p class=".$wydarzenie['home_away'].">🤦🏻‍♂️⚽️‍ ".$wydarzenie['time']."': <span class=\"gracz goal\">".strtolower($wydarzenie['player'])."</span></p>";          
+                  break;
+          case "SUBSTITUTION":
+                  echo "<p class=".$wydarzenie['home_away'].">🔄 ".$wydarzenie['time']."': ⬆ <span class=\"gracz subin\" style=\"color:#116b22;\">".$wydarzenie['player']."</span> - <span class=\"gracz subout\" style=\"color:#911919;\">".$wydarzenie['info']." </span> ⬇</p>";          
+                  break;
+
+                  
+          default:
+                echo "<p>🤯".$wydarzenie['time']."' : ".$wydarzenie['event']." - ".$wydarzenie['player']."</p>";          
+                  
+
+        }
+      }
+			?>
+
+      </div>
+<?}
+    
+      else {?>
+      <button class="notyetcollapsible rozgrywki_<?=$_score['competition_id']?>"><?=$_score['home_name']?> <?=$_score['score']?> <?=$_score['away_name']?> | <?=$_score['time']?>'
       </button>  
       <? }}?>
-</div>
-<h3 style="margin-top:15px; margin-bottom: 15px">Na żywo:</h3>
 <div>
 <p align="right" style="font-size:11px; padding-right: 10px">Wyniki są aktualizowane raz na minutę. Ostatnia aktualizacja <?php echo date("H:i")?></p></div>
 </div>
+<div id="result">
+
+</div>
+
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+$('.collapsible').each(function(){
+		if (sessionStorage.getItem(this.id)=="active"){
+		this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + "px";
+		this.classList.toggle("active");
+		this.nextElementSibling.style.transitionDuration ="0s";
+	}
+
+});
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+	 localStorage.setItem(this['id'],'active');
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+		sessionStorage.setItem(this.id,"inactive");
+		content.style.transitionDuration ="0.2s";
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+		sessionStorage.setItem(this.id,"active");
+		content.style.transitionDuration ="0.2s";
+    } 
+  });
+}
+</script>
