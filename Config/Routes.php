b36e0e5fc4c07be3094dde2c8fb9860c30c2f617
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2025-01-26 10:14:17 +0000

    Assistant checkpoint: Update routes to use uppercase HTTP methods
    Assistant generated file changes:
    - Config/Routes.php: Update HTTP methods to uppercase
    
    ---
    
    User prompt:
    
    To jest treść loga:

--- Config/Routes.php
+++ Config/Routes.php
@@ -31,21 +31,21 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('zegarek', 'LiveScore::zegarek');
-$routes->get('live/(:num)', 'LiveScore::naZywo/$1');
-$routes->get('aktywuj/(:any)', 'Auth::confirm/$1');
+$routes->GET('zegarek', 'LiveScore::zegarek');
+$routes->GET('live/(:num)', 'LiveScore::naZywo/$1');
+$routes->GET('aktywuj/(:any)', 'Auth::confirm/$1');
 $routes->get('dejnowehaslo/(:any)','Auth::newPassStart/$1');
-$routes->get('live', 'LiveScore::naZywo');
-$routes->get('test', 'LiveScore::test');
-$routes->get('wydarzenia','LiveScore::wydarzeniaMeczu');
-$routes->get('wydarzenia/(:num)','LiveScore::wydarzeniaMeczu/$1');
-$routes->get('/komentarz', 'LiveScore::komentarz');
+$routes->GET('live', 'LiveScore::naZywo');
+$routes->GET('test', 'LiveScore::test');
+$routes->GET('wydarzenia','LiveScore::wydarzeniaMeczu');
+$routes->GET('wydarzenia/(:num)','LiveScore::wydarzeniaMeczu/$1');
+$routes->GET('/komentarz', 'LiveScore::komentarz');
 $routes->get('/komentarzDoTypera', 'Typer::komentarz');
 $routes->get('/nowykomentarz', 'Komentarz::post');
 $routes->get('/zasady', 'Typer::pokazZasady');
-$routes->get('/', 'LiveScore::index');
+$routes->GET('/', 'LiveScore::index');
 $routes->get('archiwum', 'Archiwum::index');
-$routes->get('eksperyment', 'LiveScore::eksperyment');
+$routes->GET('eksperyment', 'LiveScore::eksperyment');
 $routes->get('premecz/(:num)/(:num)/(:num)','LiveScore::preMecz/$1/$2/$3');
 $routes->get('przeliczMecz/(:num)','Serwisant::policzPunktyDlaMeczu/$1');
 $routes->get('/mecze/(:num)/(:num)', 'JsonFileController::serveJson/$1/$2');
@@ -58,7 +58,7 @@ $routes->get('/cookie', 'Kalkulator::dejCookie');
 /*
 * Routing związany z byciem zalogowanym użytkownikiem
 */
-    
+
 $routes->group('', ['filter'=>'authcheck'],function($routes){
     $routes->get('typowanie', 'Typer::theGame');
     $routes->get('wszystkieMecze', 'Typer::wszystkieMecze');
