185b15053344e591656a4c75e2bf195fb6aaad4d
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2026-05-12 10:18:11 +0200

    Upiększając. Nieco. ciut. tu i ówdzie.

--- Views/administracja/kampanie.php
+++ Views/administracja/kampanie.php
@@ -1,12 +1,17 @@
 <!doctype html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Typer | Kampanie email</title>
-    <link href="<https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css>" rel="stylesheet">
-    <script src="<https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js>"></script>
+    <title>A po maturze, chodziłem wysyłać maile</title>
+    <link href="<https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css>" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="<https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js>" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
+
+
+
+
 <body class="p-4">
 
 <?php
