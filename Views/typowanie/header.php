122abb696e51e944f2aa4f084ff6b429deb7d8ff
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2026-05-15 14:08:35 +0200

    Czy to tu się skarbek mój chowa?

--- Views/typowanie/header.php
+++ Views/typowanie/header.php
@@ -5,7 +5,7 @@
     <base href="<?= base_url(); ?>">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Typer na Euro 2024</title>
+    <title><?=esc($pageTitle); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="/public/nowystyl_alpha_017.css">
