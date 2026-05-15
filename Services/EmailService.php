5c45207e35dcff3052adf2f061574f73ceb0cd01
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2026-05-15 23:07:45 +0200

    no tak to zróbmy.

--- Services/EmailService.php
+++ Services/EmailService.php
@@ -203,7 +203,7 @@ public function sendCampaign(string $templateFile, string $subject, string $targ
     foreach ($recipients as $user) {
         $personalHtml = str_replace('{nick}', esc($user['nick'] ?? ''), $html);
         if ($this->postmark->sendEmail(
-            'ogloszenia@jakiwynik.com', $user['email'], '', $subject, $personalHtml, '', 'broadcast'
+            'ogloszenia@jakiwynik.com', $user['email'], 'wit@jakiwynik.com', $subject, $personalHtml, '', 'broadcast'
         )) {
             $sent++;
         }
