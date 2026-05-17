5803af3a2d6b4f000d4738ae58ec1a8cabed119e
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2026-05-17 14:34:38 +0200

    mała różnica, raczej nie tu

--- Views/typowanie/header.php
+++ Views/typowanie/header.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
   <title><?= esc($title) ?> | Typer</title>
   <link rel="canonical" href="<?= site_url() ?>">
-  <link rel="preconnect" href="<https://fonts.googleapis.com">
-  <link rel="preconnect" href="<https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
