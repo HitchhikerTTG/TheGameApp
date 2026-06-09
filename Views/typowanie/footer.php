93aba865aa7169daedf7a2672823ae6f7a3d2c06
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2026-06-09 10:08:22 +0200

    poprawka na dole typera
    Żeby dobrze się link widział i klikał i w ogole

--- Views/typowanie/footer.php
+++ Views/typowanie/footer.php
@@ -1,7 +1,7 @@
 </div><!-- /px-3 -->
 <footer class="text-center py-4 px-3" style="border-top:1px solid var(--bs-border-color);font-size:13px;color:var(--bs-tertiary-color);">
-  Dziergane sobie w 2024 i 2026 &nbsp;·&nbsp;
-  <a href="<https://buycoffee.to/wit>" target="_blank" style="color:var(--ty-accent);">Postaw kawę ☕</a>
+  <p>Dziergane sobie w 2024 i 2026 </p>
+  <p><a href="https://buycoffee.to/wit" target="_blank" style="color:var(--ty-accent);">Jeśli dobrze się bawisz - Postaw kawę ☕</a></p>
 </footer>
 </body>
 </html>
