3a2fe9701e2b49ddce099eae904776081fba895e
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2026-06-22 22:50:53 +0200

    to też chcę dodać

--- Views/typowanie/footer.php
+++ Views/typowanie/footer.php
@@ -1,6 +1,6 @@
 </div><!-- /px-3 -->
 <footer class="text-center py-4 px-3" style="border-top:1px solid var(--bs-border-color);font-size:13px;color:var(--bs-tertiary-color);">
-  <p>Dziergane sobie w 2024 i 2026 </p>
+  <p>Dziergane sobie w 2024 i 2026 z pomocą <a href="/profil/elmo">elMo</a></p>
   <p><a href="https://buycoffee.to/wit" target="_blank" style="color:var(--ty-accent);">Jeśli dobrze się bawisz - Postaw kawę ☕</a></p>
 </footer>
 </body>
