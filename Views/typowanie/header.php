5099d1f8317a3b6bb2beb68064ba8ddc3e79d686
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2026-06-25 08:25:05 +0200

    chcąć wyjść z impasu

--- Views/typowanie/header.php
+++ Views/typowanie/header.php
@@ -4,7 +4,7 @@
   <base href="<?= base_url(); ?>">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-  <title><?= esc($title) ?> | Typer</title>
+  <title><?= STAGE | esc($title) ?> | Typer</title>
   <link rel="canonical" href="<?= site_url() ?>">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
